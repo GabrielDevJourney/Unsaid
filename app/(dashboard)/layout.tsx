e84a82a60fa7e4507e553e110ae8a578f7bd0508
Gabriel Pereira <gabspereira178@gmail.com>
2026-02-04 19:09:09 +0000

    feat(sidebar): full sidebar implementation
    Started with it in one file but when the desired styling and layout was achieved everything was
    organized and separated to achieve current implemention this way prevents duplication better
    organized and DRY principles for later changes and debugging faster iteration

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,19 @@
-const DashboardLayout = ({
+import { AppSidebar } from "@/components/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+
+const Layout = ({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) => {
     return (
         <div className="flex min-h-screen">
-            <aside className="w-60 border-r bg-sidebar" />
-            <main className="flex-1 p-6">{children}</main>
+            <SidebarProvider>
+                <AppSidebar />
+                <main className="flex-1 p-6">{children}</main>
+            </SidebarProvider>
         </div>
     );
 };
 
-export default DashboardLayout;
+export default Layout;
---
+++ components/sidebar/sidebar-user.tsx
@@ -0,0 +1,30 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+interface SidebarUserProps {
+    name: string;
+    email: string;
+    avatarUrl?: string;
+    initials: string;
+}
+
+export const SidebarUser = ({
+    name,
+    email,
+    avatarUrl,
+    initials,
+}: SidebarUserProps) => {
+    return (
+        <div className="flex items-center border-t gap-3 p-6 group-data-[state=collapsed]:justify-center">
+            <Avatar className="size-10 group-data-[state=collapsed]:size-8">
+                <AvatarImage src={avatarUrl} alt={`${name} avatar`} />
+                <AvatarFallback className="bg-neutral-700 text-white">
+                    {initials}
+                </AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col group-data-[state=collapsed]:hidden">
+                <span className="text-sm font-medium">{name}</span>
+                <span className="text-xs text-muted-foreground">{email}</span>
+            </div>
+        </div>
+    );
+};
