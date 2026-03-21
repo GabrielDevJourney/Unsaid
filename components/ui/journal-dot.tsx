fc7c0ca9ba9b6909e0fe8b99a70a7b50054e9510
Gabriel Pereira <gabspereira178@gmail.com>
2026-03-21 09:46:05 +0000

    fix(calendar): fix tooltip appearing on journaled days

--- components/ui/journal-dot.tsx
+++ components/ui/journal-dot.tsx
@@ -1,5 +1,15 @@
-const JournalDot = () => (
-    <div className="relative size-5 shrink-0 rounded-full border-[3px] border-zinc-300 bg-neutral-500" />
-);
+import { forwardRef } from "react";
+
+const JournalDot = forwardRef<
+    HTMLDivElement,
+    React.ComponentPropsWithoutRef<"div">
+>((props, ref) => (
+    <div
+        ref={ref}
+        {...props}
+        className="relative size-5 shrink-0 rounded-full border-[3px] border-zinc-300 bg-neutral-500"
+    />
+));
+JournalDot.displayName = "JournalDot";
 
 export { JournalDot };
