2a4992ccaf067da760c7a9c5af24a07355181cc7
Gabriel Pereira <gabspereira178@gmail.com>
2026-04-14 19:44:14 +0100

    feat(admin): add email review endpoint and admin view

---
+++ app/(dashboard)/backstage/emails/page.tsx
@@ -0,0 +1,7 @@
+import { EmailReviewAdminView } from "@/components/admin/email-review-admin-view";
+
+const AdminEmailsPage = async () => {
+    return <EmailReviewAdminView />;
+};
+
+export default AdminEmailsPage;
